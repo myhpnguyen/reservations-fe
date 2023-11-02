251bafd3b266d817780749ad0d60dda73225b508
myhpnguyen <116761525+myhpnguyen@users.noreply.github.com>
2023-11-02 11:11:06 -0500

    updated  homepage

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,53 @@
 <template>
-  <nav>
-    <router-link to="/">Coop's Smokehouse</router-link> 
-    <router-link to="/">Home</router-link> |
-    <router-link to="/menu">Menu</router-link> |
-    <router-link to="/photos">Photos</router-link> |
-    <router-link to="/Register">Login/Register</router-link>
-  </nav>
-  <router-view/>
-  <img :src="require('@/assets/food.jpeg')" class="card-img-top" style="width: 900px;">
+  <div class="topnav">
+  <a href="#home">Smokey's Barbeque </a>
+  <a href="#login" class="split">Login/Register</a>
+  <a href="#reserve" class="split">Reserve</a>
+  <a href="#menu" class="split">Menu</a>
+  <a href="#home" class="split">Home</a>
+</div>
+  <div class="logo"></div>
+  <!-- <img class="logo" :src="require('@/assets/logo.png')" style="display:block; margin-left: auto; margin-right: auto;"> -->
+  <!-- <img :src="require('@/assets/smokeys_menu.png')" class="card-img-top" style="width: auto; height: auto;"> -->
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/* Create a top navigation bar with a black background color  */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
   text-align: center;
-  color: #2c3e50;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 20px;
 }
 
-nav {
-  padding: 30px;
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+/* Create a right-aligned (split) link inside the navigation bar */
+.topnav a.split {
+  float: right;
+  background-color: #aa3904;
+  color: white;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.logo {
+  width: 100%;
+  height: 0;
+  padding-bottom: 30%;
+  background-image: url(http://localhost:8080/img/logo.89d83aa6.png);
+  background-size: contain;
+  background-position: 100% 100%;
+  background-repeat: round;
 }
 </style>
