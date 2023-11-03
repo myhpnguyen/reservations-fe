3ca08bd5a5df9942d13484a26e0ba66b8ba40c06
myhpnguyen <116761525+myhpnguyen@users.noreply.github.com>
2023-11-03 00:58:28 -0500

    update 2  homepage

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="topnav">
-  <a href="#home">Smokey's Barbeque </a>
-  <a href="#login" class="split">Login/Register</a>
-  <a href="#reserve" class="split">Reserve</a>
-  <a href="#menu" class="split">Menu</a>
-  <a href="#home" class="split">Home</a>
-</div>
+  <header>
+    <nav>
+        <!-- Secondary Navigation inside <header> -->
+              <div class="topnav">
+                <a href="#home">Smokey's Barbeque </a>
+                <button class="button-split" href="#login">Login/Register</button>
+                <button class="button-split" href="#reserve">Reserve</button>
+                <button class="button-split" href="#menu">Menu</button>
+                <button class="button-split" href="#home">Home</button>
+            </div>
+    </nav>
+</header>
   <div class="logo"></div>
   <!-- <img class="logo" :src="require('@/assets/logo.png')" style="display:block; margin-left: auto; margin-right: auto;"> -->
-  <!-- <img :src="require('@/assets/smokeys_menu.png')" class="card-img-top" style="width: auto; height: auto;"> -->
+
+<div class="button-container">
+  <!-- <button class="button">Menu</button> -->
+  <a class="button" href="path-to-your-pdf.pdf" target="_blank">Menu</a>
+  <button class="button">Photos</button>
+  <button class="button">Reviews</button>
+</div>
+
+<!-- <img :src="require('@/assets/smokeys_menu.png')" class="card-img-top"> -->
+
 </template>
 
 <style>
@@ -35,19 +49,59 @@
 }
 
 /* Create a right-aligned (split) link inside the navigation bar */
-.topnav a.split {
+/* .topnav a.split {
   float: right;
   background-color: #aa3904;
   color: white;
-}
+} */
 
 .logo {
   width: 100%;
   height: 0;
   padding-bottom: 30%;
-  background-image: url(http://localhost:8080/img/logo.89d83aa6.png);
-  background-size: contain;
-  background-position: 100% 100%;
-  background-repeat: round;
+  background-image: url('@/assets/logo2.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
+
+.button-container {
+  text-align: center; 
+  margin-top: 20px; 
+}
+
+.button {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color:#aa3904;
+;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  margin: 5px;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.button-split {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color:#aa3904;
+;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  margin: 5px;
+  text-decoration: none;
+  cursor: pointer;
+  background-color: #aa3904;
+  color: white;
+  float: right;
+}
+
+.card-img-top {
+  height: fit-content;
+  width: fit-content;
+}
+
 </style>
